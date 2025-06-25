f368f1b8b0c6958d9adfe3044dba7925e46515a8
Parvez-hasan <parvezp7788@gmail.com>
2025-06-25 07:12:35 -0700

    same add

--- ternary.js
+++ ternary.js
@@ -1,6 +1,46 @@
 // basic ternary
-
+/*
 let num = 10;
 let result = num > 5 ?    'positive' : 'negaive';
 let results = num < 5 ?   'positive' : 'negaive';
 console.log(result,results);
+
+
+// Vote by Age
+var age = 18
+var message = age >= 18 ? 'can vote' : 'too young to vote';
+var messages = age <= 14 ? 'can vote' : 'too young to vote';
+console.log(message,messages);
+
+// String
+let Name = "Parvez Hasan";
+const Professtion = 'I am professional web devolaper';
+
+// length
+var ward = "programming"
+console.log(ward.length);
+
+
+// ** toUpperCase ** 
+
+var Name = 'parvez hasan';
+let Name1 ='lets code your career';
+console.log(Name.toUpperCase ());
+console.log(Name1.toUpperCase ());  */
+
+
+// ** toLowerCase **
+
+let myTown = "KISSHOREGANJ"
+const 
+
+
+
+
+
+
+
+
+
+
+
