3bee701d4b1adc267a711955e264b8f6f937fc20
Parvez-hasan <parvezp7788@gmail.com>
2025-06-25 08:55:50 -0700

    same ternary opatetor add

--- ternary.js
+++ ternary.js
@@ -21,18 +21,68 @@ var ward = "programming"
 console.log(ward.length);
 
 
-// ** toUpperCase ** 
+// *** toUpperCase *** 
 
 var Name = 'parvez hasan';
 let Name1 ='lets code your career';
 console.log(Name.toUpperCase ());
-console.log(Name1.toUpperCase ());  */
+console.log(Name1.toUpperCase ());  
 
 
-// ** toLowerCase **
+// *** toLowerCase ***
 
-let myTown = "KISSHOREGANJ"
-const 
+let myTown = "KISSHOREGANJ";
+const dream = 'LETS CODE YOUR CAREER';
+console.log(myTown.toLowerCase());
+console.log(dream.toLowerCase()); 
+
+
+// *** includs ***
+
+let identity = "Hello,I am parvez hasan. I am professional web developer. I have good experience in this work";
+
+console.log(identity.includes("parvez hasan"));
+console.log(identity.includes("Developer"));
+console.log(identity.includes("experience")); 
+
+
+// * indexOF *
+
+let food = "banana";
+console.log(food.indexOf("n")); 
+
+
+// * slice *
+
+let it = "Programming Hero";
+console.log(it.slice(0, 5 ));
+console.log(it.slice(8, 16)); 
+
+
+// * substring *
+
+let java = "JavaScript";
+console.log(java.substring(4, 10)); 
+
+// * replace..old & new *
+
+let web = "i love programming language";
+console.log(web.replace("programming language","non-coding language"));  */
+
+
+// *** Trim ***
+
+let 
+
+
+
+
+
+
+
+
+
+
 
 
 
